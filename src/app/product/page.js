c9961318931c27902d2e07fbcb1231267f0235f0
guidofrassetti <guidofrassetti@gmail.com>
2025-06-06 05:02:30 +0000

    update

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import ProductPageContent from "@/components/ui/ProductPageContent";
+import ProductPageContent from "@/components/ProductPageContent";
 
 export default function Page() {
   return (
