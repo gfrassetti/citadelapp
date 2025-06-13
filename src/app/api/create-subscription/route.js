5cc1726e029f71447aba3be280ae893b8b89fe2b
guidofrassetti <guidofrassetti@gmail.com>
2025-06-13 05:20:38 +0000

    env vars for testing

--- src/app/api/create-subscription/route.js
+++ src/app/api/create-subscription/route.js
@@ -43,7 +43,7 @@ const api = {
               currency_id: "ARS",
               start_date: startDate.toISOString(),
             },
-            payer_email: "test_user_895208562@testuser.com", //email para prod
+            payer_email: "test_user_895208562@testuser.com", //email
             back_url: "https://admin-panel-psi-two.vercel.app/dashboard", //cambiar
             external_reference: uid,
           },
