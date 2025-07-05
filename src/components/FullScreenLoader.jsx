864568d52684421c6ffc5719c70cbbcd58e8abcc
guidofrassetti <guidofrassetti@gmail.com>
2025-07-05 22:27:41 +0000

    fix ya sin retorno alguno

--- src/components/FullScreenLoader.jsx
+++ src/components/FullScreenLoader.jsx
@@ -2,7 +2,7 @@
 
 export default function FullScreenLoader() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-full bg-white text-black z-20">
+    <div className="loaderscreen flex flex-col justify-center items-center h-screen w-full bg-white text-black z-20" style={{zIndex: '20'}}>
       <div className="text-xl font-medium">Cargando tu experiencia personalizada...</div>
       <div className="mt-4 animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-black" />
     </div>
