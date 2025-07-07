141ae8f3b49db9f2b1e87fef9e681104c5e0e089
guidofrassetti <guidofrassetti@gmail.com>
2025-07-07 13:07:17 +0000

    fix 3

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,22 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import { useUser } from '@/context/AuthContext';
 import ProDashboardPanel from '@/components/ProDashboardPanel';
 import FreeDashboardPanel from '@/components/FreeDashboardPanel';
-import {DashboardBreadcrumb} from '@/components/DashboardBreadcrumb';
+import { DashboardBreadcrumb } from '@/components/DashboardBreadcrumb';
 import FullScreenLoader from '@/components/FullScreenLoader';
 
 export default function DashboardHome() {
   const { user, loading } = useUser();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      console.log('Redirigiendo al login...');
-      router.replace('/login');
-    }
-  }, [loading, user]);
 
   if (loading || !user) return <FullScreenLoader />;
 
