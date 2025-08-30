06db01b9fe57f53732daac8c9269d7b128abef57
guidofrassetti <guidofrassetti@gmail.com>
2025-08-30 16:20:15 +0000

    page js dashboard cambio de state de local a global

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,17 +1,23 @@
-'use client';
+"use client";
 
-import { useUser } from '@/context/AuthContext';
-import ProDashboardPanel from '@/components/ProDashboardPanel';
-import FreeDashboardPanel from '@/components/FreeDashboardPanel';
-import { DashboardBreadcrumb } from '@/components/DashboardBreadcrumb';
+import ProDashboardPanel from "@/components/ProDashboardPanel";
+import FreeDashboardPanel from "@/components/FreeDashboardPanel";
+import { DashboardBreadcrumb } from "@/components/DashboardBreadcrumb";
+import { useSubscription } from "@/context/SubscriptionContext";
+
 
 export default function DashboardHome() {
-  const { user } = useUser();
+  const { subscription, loading: subscriptionLoading } = useSubscription();
+
+  const isPro =
+    subscription?.status === "active" && !subscription?.pause_collection;
+
+  if (subscriptionLoading) return null; // o loader
 
   return (
     <>
       <DashboardBreadcrumb />
-      {user.plan === 'free' ? <FreeDashboardPanel /> : <ProDashboardPanel />}
+      {isPro ? <ProDashboardPanel /> : <FreeDashboardPanel />}
     </>
   );
 }
