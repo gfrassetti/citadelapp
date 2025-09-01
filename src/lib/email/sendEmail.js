2e5f9ffcf249a985ef8f0859c65c2ab5742f0e88
guidofrassetti <guidofrassetti@gmail.com>
2025-09-01 15:32:42 +0000

    sendemial js

--- src/lib/email/sendEmail.js
+++ src/lib/email/sendEmail.js
@@ -1,10 +1,13 @@
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+"use server";
+import { Resend } from "resend";
 
 export async function sendEmail({ to, subject, html }) {
+  const apiKey = process.env.RESEND_API_KEY; // se lee dentro de la función
+  if (!apiKey) throw new Error("Missing RESEND_API_KEY");
+
+  const resend = new Resend(apiKey);
   return resend.emails.send({
-    from: 'La Citadel <no-reply@lacitadel.com.ar>',
+    from: "La Citadel <no-reply@lacitadel.com.ar>",
     to,
     subject,
     html,
